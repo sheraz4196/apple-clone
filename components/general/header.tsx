84395caac87e18f2d2308d3896d164e00c6cfacb
sheraz4196 <sheraztoor321@gmail.com>
2024-07-11 18:36:11 +0500

    feat: logo

--- components/general/header.tsx
+++ components/general/header.tsx
@@ -1,3 +1,9 @@
+import Image from "next/image";
+
 export default function Header() {
-  return <header></header>;
+  return (
+    <header className="px-6 py-3 max-w-screen-lg mx-auto">
+      <Image src={"/assets/logo.png"} width={17} height={48} alt="Apple Logo" />
+    </header>
+  );
 }
